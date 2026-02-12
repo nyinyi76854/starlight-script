4dc7d0cebb27ba2e4412b5ef1f5fd98c8c8a1e00
nyinyi76854 <148785112+nyinyi76854@users.noreply.github.com>
2026-02-12 07:49:25 +0630

    Update lexer.js

--- lexer.js
+++ lexer.js
@@ -1,14 +1,10 @@
-// src/lexer.js
-
 (function () {
 
-  if (!window.Starlight) {
-    window.Starlight = {};
-  }
+  window.Starlight = window.Starlight || {};
 
   function tokenize(input) {
     const tokens = [];
-    const regex = /\s*(let|print|whereElement|set|\d+|"[^"]*"|\+|\-|\*|\/|=|\(|\)|\.|;|[A-Za-z_]\w*|#[A-Za-z_]\w*)\s*/g;
+    const regex = /\s*(let|print|whereElement|set|\d+|"[^"]*"|\+|\-|\*|\/|=|\(|\)|\.|;|[A-Za-z_]\w*)\s*/g;
 
     let match;
     while ((match = regex.exec(input)) !== null) {
